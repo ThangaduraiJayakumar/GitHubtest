6722e5140d1adef0f539850606f5b7a68f0d5fe8
ThangaduraiJayakumar <jaidurai773@gmail.com>
2018-11-21 01:04:53 +0530

    Githubtest1

---
+++ src/org/test/GitHub.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class GitHub {
+private void test() {
+	// TODO Auto-generated method stub
+System.out.println("GitHub Test");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+GitHub gh = new GitHub();
+gh.test();
+	}
+
+}
